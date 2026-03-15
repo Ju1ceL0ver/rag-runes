63f13d7f6a18a44ae7616bd7ebd41643562fc22f
Aleksei <zagorskii.aleksei.2004@gmail.com>
2026-03-15 16:39:28 +0300

    initial commit

--- rag_runes/__init__.py
+++
@@ -1,4 +0,0 @@
-"""rag_runes package."""
-
-__all__ = ["__version__"]
-__version__ = "0.1.0"
--- rag_runes/ocr/base.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from typing import Protocol
-
-
-class OCRClient(Protocol):
-    def extract_text(self, image_path: str) -> str:
-        ...
